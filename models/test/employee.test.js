623c12e8176694bc19afaa763f24948a1582c81e
Dorota Tomala <doforever@gmail.com>
2021-05-05 11:59:45 +0200

    Test department model

---
+++ models/test/employee.test.js
@@ -0,0 +1,11 @@
+const Department = require('../department.model.js');
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+
+describe('Department', () => {
+  after(() => {
+    mongoose.models = {};
+  });
+
+  
+});
