dbb284537e748daa9a676a3da297e93af0342bd3
Dorota Tomala <doforever@gmail.com>
2021-05-06 11:03:21 +0200

    Test get department requests

--- models/test/employee.crud.test.js
+++ models/test/employee.crud.test.js
@@ -31,6 +31,7 @@ describe('Employee', () => {
 
     after(async () => {
       await Employee.deleteMany();
+      await Department.deleteMany();
     });
 
     it('should return all the data with "find" method', async () => {
